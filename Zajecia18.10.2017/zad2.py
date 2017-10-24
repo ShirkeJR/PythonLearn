4a4e521e4e234753700a21416ecad8b9b4837433
Jakub Rycyk <jakub.rycyk@outlook.com>
2017-10-24 20:53:29 +0200

    Dodanie zad2.py

--- Zajecia18.10.2017/zad2.py
+++ Zajecia18.10.2017/zad2.py
@@ -1,7 +1,8 @@
 def fibo(liczba):
-    return liczba
-
+    fibonacci_numbers = [0, 1]
+    [fibonacci_numbers.append(fibonacci_numbers[i-1]+fibonacci_numbers[i-2]) for i in range(2, liczba)]
+    return fibonacci_numbers
+    
 liczba = input('Podaj liczbe elementow ciagu: ')
-print fibo(liczba)
+print fibo(liczba+1)
 
-# do domu
