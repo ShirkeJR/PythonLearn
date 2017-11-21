8ac92ec7b2990fa9b97cc8374e3f2370dfc0206f
rycykj <jakub.rycyk@outlook.com>
2017-11-21 22:15:27 +0100

    Fix Zajecia15.11.2017

--- Zajecia15.11.2017/zad5.py
+++ Zajecia15.11.2017/zad5.py
@@ -1,10 +1,72 @@
-# materialy sa na dysku gdrive
+import sys
+import json
 
-tytul = raw_input("Tytul: ")
-isbn = input("Numer ISBN: ")
-autor = raw_input("Autor: ")
+class Ksiazka(dict):
+    def __init__(self, tytul, isbn, autor):
+        self.tytul = tytul
+        self.isbn = isbn
+        self.autor = autor
 
-slownik = {}
-slownik = isbn[tytul, autor]
 
-print slownik[isbn]
+if len(sys.argv) == 1:
+    exit(1)
+
+
+
+obiekty = []
+
+
+def dodawanie():
+    a = Ksiazka(raw_input('Podaj tytul: '), raw_input('Podaj ISBN: '), raw_input('Podaj autora: '))
+    obiekty.append(a)
+
+
+def wyswietlanie():
+    for ksiazka in obiekty:
+        print ksiazka.autor, ksiazka.tytul, ksiazka.isbn
+
+
+def usuwanie(ksiazki):
+    isbn = raw_input('Podaj ISBN ksiazki do usuniecia')
+    obiekty = [ksiazka for ksiazka in ksiazki if ksiazka.isbn != isbn]
+
+
+def zapis():
+    with open(sys.argv, 'w') as outfile:
+        json.dumps(obiekty, outfile)
+
+
+def odczyt():
+    with open(plik, 'r') as infile:
+        obiekty = json.loads(infile)
+
+
+def loop():
+    print '------------------------------'
+    print '1. Dodawanie'
+    print '2. Wyswietlanie'
+    print '3. Usuwanie'
+    print '4. Zapis'
+    print '5. Odczyt'
+    print '6. Wyjscie'
+    wybor = input('Wybierz opcje: ')
+    if wybor == 1:
+        dodawanie()
+    elif wybor == 2:
+        wyswietlanie()
+    elif wybor == 3:
+        usuwanie(obiekty)
+    elif wybor == 4:
+        zapis()
+    elif wybor == 5:
+        odczyt()
+    else:
+        exit(0)
+
+    loop()
+
+
+loop()
+
+
+
