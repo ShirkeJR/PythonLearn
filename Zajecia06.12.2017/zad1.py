39c52759949151ec0201677b8af09a61dda96f7d
Jakub Rycyk <jakub.rycyk@outlook.com>
2017-12-13 12:42:29 +0100

    [ref] code

--- Zajecia06.12.2017/zad1.py
+++ Zajecia06.12.2017/zad1.py
@@ -5,20 +5,35 @@ import tkMessageBox
 def helloButton1():
     tkMessageBox.showinfo("","Hello, here Tkinter")
 
-def buttonColor():
-    b1.configure(foreground = text)
+def ChangeButtonColour():
+   if(var.get() == 1):
+       b.config(fg="red")
+   elif(var.get() == 2):
+       b.config(fg="green")
+   elif(var.get() == 3):
+       b.config(fg="blue")
+       
+def SelectColour():
+   if(var.get() == 1):
+       return "red"
+   elif(var.get() == 2):
+       return "green"
+   elif(var.get() == 3):
+       return "blue"
+
 
 root = Tk()
 root.title("Hello Tkinter")
-b1 = Button(root, text = "Witam", command = helloButton1).pack()
+root.geometry("200x100")
+b1 = Button(root, text = "Witam", fg = "balck", command = helloButton1).pack()
 
 MODES = [
         ("green", 1),
         ("red", 2),
         ("black", 3),
     ]
-
+var = IntVar()
 for text, mode in MODES:
-    b = Radiobutton(root, text=text, variable = (b1, text), value=mode).pack(anchor=W)
+    b = Radiobutton(root, text=text, variable = var, value=mode, command = ChangeButtonColour).pack(anchor=W)
 label = Label(root).pack()
 root.mainloop()
--- Zajecia06.12.2017/zad2.py
+++ Zajecia06.12.2017/zad2.py
@@ -3,18 +3,29 @@ from Tkinter import *
 import tkMessageBox
 
 
-def helloButton1():
-    tkMessageBox.showinfo("","Czas sie skonczyl")
-
-def buttonStart():
-    pass
-
-
+def ShowTimer():
+   s = int(e.get())
+   
+   if(s == 0 or s < 0):
+       messagebox.showinfo("Message", "Time has expired!")
+       return
+   else:
+       e.delete(0, END)
+       s -= 1
+       e.insert(END, s)
+       print(s)
+       
+   e.after(1000,ShowTimer)
+       
+def Clear():
+   e.delete(0, END)
+   
 root = Tk()
-root.title("Hello Tkinter")
-label = Label(root).configure(text = "100")
-
-b1 = Button(root, text = "Witam", variable = label, command = buttonStart).pack()
-
-
+root.title("Timer")
+e = Entry(root)
+b1 = Button(root, text="Start", command=ShowTimer)
+b2 = Button(root, text="Clear", command=Clear)
+e.grid(row = 0, column = 0, columnspan=2)
+b1.grid(row = 1, column = 0)
+b2.grid(row = 1, column = 1)
 root.mainloop()
